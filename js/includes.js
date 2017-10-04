ef4f7975c56c9c7adcaa213e99e394c035f1dd71
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-10-03 21:16:46 -0400

    Add active class to current page in drop down

--- js/includes.js
+++ js/includes.js
@@ -1,6 +1,9 @@
 $(function() {
     $.get("includes/header.html", function(data) {
         $('body').prepend(data);
+        $('.dropdown-item[href="' + window.location.pathname.split('/').pop() + '"]').addClass('active');
     });
     $("#footer").load("includes/footer.html");
+
+
 })
